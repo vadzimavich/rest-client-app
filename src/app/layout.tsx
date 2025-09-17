5eccde40f159f7b21252b1689194f63db71ceaed
Natasha <natalyamerr@gmail.com>
2025-09-18 00:28:49 +0300

    fix: fix layout according to eslint warns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { AuthProvider } from "@/components/providers/AuthProvider";
-import Layout from "@/components/mainLayout/MainLayout";
+import './globals.css';
+import { AuthProvider } from '@/components/providers/AuthProvider';
+import Layout from '@/components/mainLayout/MainLayout';
+import { geistSans, geistMono } from '@/lib/fonts';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "REST Client App",
-  description: "Lightweight Postman-like client",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} layout`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} app-layout`}>
         <AuthProvider>
           <Layout>{children}</Layout>
         </AuthProvider>
---
+++ src/lib/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "REST Client App",
+  description: "Lightweight Postman-like client",
+};
