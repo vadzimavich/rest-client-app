fc0549b71c7f8b7e22b782b5b73de1b866d937d4
Natasha <natalyamerr@gmail.com>
2025-09-21 21:07:11 +0300

    feat: add method selector styles

---
+++ src/components/methodSelector/MethodSelector.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { HttpMethod } from "@/types/request";
+import styles from './MethodSelector.module.css';
+
+interface MethodSelectorProps {
+  value: HttpMethod;
+  onChange: (method: HttpMethod) => void;
+}
+
+export default function MethodSelector({ value, onChange }: MethodSelectorProps) {
+  const methods: HttpMethod[] = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+  return (
+    <select
+      className={styles.methodSelector}
+      value={value}
+      onChange={(e) => onChange(e.target.value as HttpMethod)}
+    >
+      {methods.map(method => (
+        <option key={method} value={method}>{method}</option>
+      ))}
+    </select>
+  );
+}
