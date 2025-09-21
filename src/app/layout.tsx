bf1b09e9bba73139338cbbaf434ba83ef0a27a93
Natasha <natalyamerr@gmail.com>
2025-09-22 00:45:41 +0300

    feat: add toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { AuthProvider } from '@/components/providers/AuthProvider';
 import Layout from '@/components/mainLayout/MainLayout';
 import { geistSans, geistMono } from '@/lib/fonts';
+import ToastProvider from '@/components/toastProvider/ToastProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} app-layout`}>
         <AuthProvider>
           <Layout>{children}</Layout>
+          <ToastProvider />
         </AuthProvider>
       </body>
     </html>
